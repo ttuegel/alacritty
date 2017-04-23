d6a2d39d0ddb0157d70dce143d0f9e0fa5ce83c9
Prashant Vaibhav <prashant.vaibhav@tobyrich.com>
2017-04-23 14:17:02 +0200

    Use a better "underline" character than underscore
    Works around the hacky underlining which just renders an underscore
    character behind the letter. This underscore character looks terrible on
    many fonts, (e.g. Iosevka) and is actually confusing since it makes
    spaces seem like underscores.
    
    I just replaced that character with a better underline character that's
    present in the Iosevka Nerd Font. This actually looks exactly like an
    underscore would look (except for the fact that it doesn't break the
    line for letters like y. If the user doesn't have a font that includes
    this character (it's in Unicode private usage plane), then underline
    won't be displayed. This is still better than layering an underscore.
    
    (cherry picked from commit 4695a9d7f400224d557f3cf542325d7d627d9dce)

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -853,7 +853,7 @@ impl<'a> RenderApi<'a> {
                 let glyph_key = GlyphKey {
                     font_key: font_key,
                     size: glyph_cache.font_size,
-                    c: '_'
+                    c: ''
                 };
 
                 let underscore = glyph_cache.get(&glyph_key, self);
